91a2a3b4f3c4ad4b3552a5e8e6e851557d42f179
baolhq <baolhq280@gmail.com>
2024-11-23 15:47:03 +0700

    Update general configurations

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    "next/core-web-vitals", "next/typescript"
   ],
   root: true,
   env: {
